868bb6b554aef8ac1d6317340984a04777a52648
Heiko Scherrer <hscherrer@interface21.io>
2021-09-04 23:31:57 +0200

    Added utils class for conversation helpers

---
+++ src/main/java/org/openwms/core/units/UnitsUtil.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2005-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.core.units;
+
+import org.openwms.core.units.api.Piece;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+/**
+ * A UnitsUtil.
+ *
+ * @author Heiko Scherrer
+ */
+public final class UnitsUtil {
+
+    private UnitsUtil() {}
+
+    public static BigDecimal sumPieces(Stream<Piece> pieceStream) {
+        return pieceStream
+                .map(s -> BigDecimal.valueOf(s.getMagnitude().floatValue()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
